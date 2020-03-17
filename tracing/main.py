7ae2995a453e8d38c7a77580513b5f3f1a5fd05a
jimlauh-oi <52490079+jimlauh-oi@users.noreply.github.com>
2020-03-17 11:27:11 +0800

    Updated frontend

---
+++ tracing/main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 10:56:23 2020
+
+@author: JimLau
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import operator
+import math
+
+
+
+def main():
+    print(math.pow(2))
+if __name__ == "__main__":
+    main()
